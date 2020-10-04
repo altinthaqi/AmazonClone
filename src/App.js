cdc256dcad42e33c91a5c0c93bb228bf412c1c5c
altin thaqi <ltin.thaqi.520@gmail.com>
2020-10-04 23:05:22 +0200

    Fixed Bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
@@ -6,20 +6,44 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import { useStateValue } from "./StateProvider";
 import Login from './Login'
+import { auth } from "./firebase"
 
 
 
 function App() {
-  const [{basket}, dispatch] = useStateValue();
+  const [{user}, dispatch] = useStateValue();
 
   //useEffect <<< Learn it
   //Piece of code which runs in a given condition
 
   useEffect(()=>{
-    
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        //user is logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser
+        })
+      }
+      else{
+        //user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        })
+      }
+    });
+
+    return () => {
+      //any cleanup will go here
+      unsubscribe();
+
+
+    }
   }, [])
 
-  
+  console.log('USER IS >>>>>>>>>' + user);
+
   return (
     <Router>
       <div className="app">
