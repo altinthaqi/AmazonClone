2255c4fe5b5075839ebb1303709cd239c2693f74
altin thaqi <ltin.thaqi.520@gmail.com>
2020-10-04 12:51:54 +0200

    Seting up React Context API (data layer)

---
+++ src/StateProvider.js
@@ -0,0 +1,14 @@
+// data layer
+// to track the basket
+
+import React, { createContext, useContext, useReducer} from "react";
+
+// THIS IS DATA LAYER
+export const StateContext = createContext();
+
+// BUILD A PROVIDER
+export const StateProvider = ({ reducer, initialState, children}) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+)
