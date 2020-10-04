c4e0d78c7d0478783ab95e8d44bf6421e5788b5c
altin thaqi <ltin.thaqi.520@gmail.com>
2020-10-04 18:59:08 +0200

    Initialised Subtotal

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { useStateValue } from './StateProvider'
 import './Checkout.css'
 import CheckoutProduct from './CheckoutProduct'
+import Subtotal from './Subtotal'
 function Checkout() {
 
     const [{basket}] = useStateValue();
 
     return (
         <div className="checkout">
+            <div className="checkout__left">
             <img className="checkout__ad" 
             src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
             alt = ""
@@ -37,6 +39,12 @@ function Checkout() {
                     
                 </div>
             )}
+        </div>
+        {basket.length > 0 &&(
+            <div className="checkout__right">
+                <Subtotal />
+                </div>
+        )}
         </div>
     )
 }
